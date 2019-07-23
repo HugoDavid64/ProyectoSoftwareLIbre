4c7a87ed040b46c78f6a7841e08df735f16fe987
Hugo Zhindon <hzhindon@est.ups.edu.ec>
2019-07-23 12:04:56 -0500

    Segundo Cambio

--- ProyectoFinal/src/proyectofinal/Proyecto.java
+++ ProyectoFinal/src/proyectofinal/Proyecto.java
@@ -5,10 +5,47 @@
  */
 package proyectofinal;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 /**
  *
  * @author david
  */
-public class Proyecto {
+public class Proyecto extends JFrame implements ActionListener {
+
+   JFrame vent, vent1,ventAcercaDe;
+    JLabel lbltitulo, lblfondo, c1, c2, c11, lblflf,j1,j2,j3,j4;
+    JButton btnIniciar, bj1, bj2, bj3, bj4, btnIniciar2;
+    Timer timer, timer2;
+    JLabel homero, p1, p2, p3, p4;
+    JLabel aviso;
+    JLabel obstaculo;
+    JLabel s1, s2, s3;
+    JMenuBar barra;
+    JMenu archivo,informacion,ayuda;
+    JMenuItem cerrarTodo, iniciar,acercad;
+    boolean ft = true;
+    int tecla, x = 200, y = 100;
+    int key;
+    String datoPro;
+    JLabel lblimg; 
     
+   
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    //CambioNuevo
 }
